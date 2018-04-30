f7c0edea74e314c5c59f5abf8e0de2dbaa794216
Wagner Mengue <dev.mengue@gmail.com>
2018-04-29 21:40:36 -0300

    change user validate

--- app/Http/Controllers/Admin/UsuariosController.php
+++ app/Http/Controllers/Admin/UsuariosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 
 class UsuariosController extends Controller
 {
@@ -87,11 +88,21 @@ class UsuariosController extends Controller
     public function update(Request $request, $id)
     {
         $data = $request->all();
-        $validacao = \Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
+
+        if(isset($data['password']) && $data['password'] != ''){
+            $validacao = \Validator::make($data, [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:6',
+            ]);
+            $data['password'] = bcrypt($data['password']);
+        }else{
+            $validacao = \Validator::make($data, [
+                'name' => 'required|string|max:255',
+                'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($id)],
+            ]);
+            unset($data['password']);
+        }
         if($validacao->fails()){
             return redirect()->back()->withErrors($validacao)->withInput();
         }
